2de107a988111160f035caa1efd7fbe7e8fe1bd8
mseaton <mseaton@pih.org>
2016-06-27 16:44:24 -0400

    Add oshi to pom and re-organize dependencies a bit

--- api/src/test/java/org/openmrs/module/emrmonitor/api/EmrMonitorServiceTest.java
+++ api/src/test/java/org/openmrs/module/emrmonitor/api/EmrMonitorServiceTest.java
@@ -19,7 +19,7 @@ import org.openmrs.api.context.Context;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 /**
- * Tests {@link ${EmrMonitorService}}.
+ * Tests EmrMonitorService
  */
 public class  EmrMonitorServiceTest extends BaseModuleContextSensitiveTest {
 	
